6f15e9f51aff17dddc7c5bb4dabeca3a1b3b7b0a
Zac M-W <contact@zac.nz>
2024-08-25 00:16:21 +1200

    Add fail screen

---
+++ src/routes/api/[user]/pages/fail/+page.svelte
@@ -0,0 +1,11 @@
+<div class="flex flex-col h-full">
+  <div class="flex grow items-center pb-2 gap-2">
+    <div>
+      <p class="daydream daydream-lg">Oh bugger!</p>
+      <p class="text-sm">
+        Something went very wrong, and your device is showing this screen to let
+        you know. I wish I could tell you what happened, but I'm just a screen.
+      </p>
+    </div>
+  </div>
+</div>
