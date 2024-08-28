57aeeac1f2e632322ea1d0bfeec04a84c050f447
Zac M-W <contact@zac.nz>
2024-08-28 08:48:15 +0100

    Add instructions

--- src/routes/(client)/device/+page.svelte
+++ src/routes/(client)/device/+page.svelte
@@ -11,6 +11,7 @@
   import { slide } from "svelte/transition";
   import { tick } from "svelte";
   import { base } from "$app/paths";
+  import bootImg from "./arduino-bootLabel.jpeg";
 
   const { Terminal } = xterm;
 
@@ -152,6 +153,60 @@
 <FrontFace page={0} live={true} userData={data.user} />
 
 <div class="flex flex-col items-center gap-4">
+  <div class="font-sans prose w-full max-w-screen-md">
+    <p>
+      Welcome to the RSB device update tool! You can use this to install the
+      latest version. <strong
+        >Heads up: you should only do this if your device isn't working, or
+        there's an update you know about.</strong
+      > To get started:
+    </p>
+    <ol class="list-decimal list-inside mt-4 mx-4 flex flex-col gap-3">
+      <li>
+        Find out whether your device uses a black and white or black, white, and
+        red display, and set the dropdown below. <strong
+          >Not sure? If your device spends 20 seconds flashing the screen when
+          you turn it on, it's black+white+red.</strong
+        >
+      </li>
+      <li>
+        Open the case of your device, and pull out the circuit board that the
+        USB plugs into
+      </li>
+      <li>
+        Find the tiny little button to the right of the USB port - you will need
+        this in a couple steps:
+        <img
+          src={bootImg}
+          alt="Illustration of where the button is"
+          class="max-w-sm mx-auto"
+        />
+      </li>
+      <li>Plug the device into your computer's USB port</li>
+      <li>Click "Update to latest" below</li>
+      <li>
+        When the message "https://rsb.nz/ wants to connect to a serial port"
+        appears, select the device with a name starting with "CP2102 USB TO
+        UART..." but DO NOT click connect.
+      </li>
+      <li>
+        <strong
+          >Hold down the tiny button you found earlier, and click connect</strong
+        >
+      </li>
+      <li>
+        Wait for the progress bar to finish and the device to make a noise, then
+        unplug! You're done.
+      </li>
+    </ol>
+    <p class="mt-4">
+      <strong
+        >Hearing one high beep then two lows, and is the screen not updating?
+        Make sure all the wires are securely plugged into the circuit board you
+        pulled out. The display might be loose!</strong
+      >
+    </p>
+  </div>
   <div class="deviceOptions">
     <select
       bind:value={screenType}
