970c61e92a020396e8685ed0016ed253606a9ecb
Jasper Miller-Waugh <contact@jmw.nz>
2024-09-02 00:48:27 +1200

    WIP github release OTA

---
+++ firmware/esp32_2-9in/src/GhOta.cpp
@@ -0,0 +1,12 @@
+#include <GhOta.h>
+
+GitHubOTA OsOta(FIRMWARE_VERSION_CODE, RELEASE_URL, "esp32-" FIRMWARE_VERSION_VARIANT "-firmware-merged.bin", true);
+
+
+void OTA_Initialise()
+{
+    // Will need to mod libaray to add the ability to skip, progress onscreen etc
+    OsOta.handle();
+}
+
+
